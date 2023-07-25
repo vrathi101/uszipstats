d4d7a8ae98bc7af69b6b9d46545c1413cff7ae3d
Vedant Rathi <vedantrathi@vedants-air.lan>
2023-07-25 13:06:51 -0500

    initial version

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+setuptools.setup(
+    name='uszipstats',         # How you named your package
+    packages=['code_zip'],   # Chose the same as "name"
+    version='0.1',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    description='sample package',   # Give a short description about your library
+    author='VEDANT RATHI',                   # Type in your name
+    author_email='vedrathi10@gmail.com',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/vrathi101/uszipstats.git',
+    # I explain this later on
+    download_url='https://github.com/vrathi101/uszipstats/archive/refs/tags/v_01.tar.gz',
+    keywords=['DATA', 'FUNCTIONS'],   # Keywords that define your package best
+    install_requires=[
+        'pandas',
+        'numpy',
+        'requests',
+        'matplotlib',
+        'json',
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
